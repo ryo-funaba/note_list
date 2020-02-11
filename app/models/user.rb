30dab01532b9f6159da037cc6ae3153569dc96e9
ryo-funaba <ryo278.0417@gmail.com>
2020-02-11 02:19:22 +0000

    setting association

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   
   has_one_attached :image 
-  
+  has_many :reviews
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
