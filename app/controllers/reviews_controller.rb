3b835404134026fb4a8353c287a4573bbdc267e0
ryo-funaba <ryo278.0417@gmail.com>
2020-02-12 07:00:20 +0000

    change review/new.slim

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,7 +3,17 @@ class ReviewsController < ApplicationController
   before_action :authenticate_user!, only: :new
   
   def new
-    
+    @review = Review.new
+  end
+  
+  def create
+    Review.create(create_params)
+    redirect_to controller: :products, action: :index
+  end
+  
+  private
+  def create_params
+    params.require(:review).permit(:review, :rate).merge(product_id: params[:product_id], user_id: current_user.id)
   end
   
 end
