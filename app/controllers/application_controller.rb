b793c49fc4f56b6fef4031f52a0ff4cabeaae483
ryo-funaba <ryo278.0417@gmail.com>
2020-02-20 12:08:14 +0000

    change config/env/deve.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,5 +13,6 @@ class ApplicationController < ActionController::Base
   
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :image])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,9 @@
 class UsersController < ApplicationController
   
   def show
-    @nickname = current_user.nickname
-    # user = User.find(params[:id])
-    # @nickname = user.nickname
-    # @products = User.reviews.notes.page(params[:page]).per(8).order("created_at DESC")
+    user = User.find(params[:id])
+    @nickname = user.nickname
+    @notes = User.reviews.notes.page(params[:page]).per(8).order("created_at DESC")
   end
   
 end
