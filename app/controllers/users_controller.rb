0c98effc39cfc2c7dc5137a0db1eafe710e8e749
ryo-funaba <ryo278.0417@gmail.com>
2020-02-21 01:02:36 +0000

    to share error of mypage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,7 @@ class UsersController < ApplicationController
   
   def show
     user = User.find(params[:id])
-    @nickname = user.nickname
-    @notes = User.reviews.notes.page(params[:page]).per(8).order("created_at DESC")
+    # notes = user.reviews.notes.page(params[:page]).per(8).order("created_at DESC")
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
     
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   root 'notes#index'
   
   resources :users, only: :show
