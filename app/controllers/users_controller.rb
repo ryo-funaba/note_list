0ca57063a84fe8ee3fe7e177b71492e3592f53a2
ryo-funaba <ryo278.0417@gmail.com>
2020-02-22 01:50:01 +0000

    アカウント編集画面で画像表示を編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   
   def show
-    user = User.find(params[:id])
+    @user = User.find(params[:id])
+    # @nickname = user.nickname
     # notes = user.reviews.notes.page(params[:page]).per(8).order("created_at DESC")
   end
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   
   has_one_attached :image 
-  has_many :reviews
+  has_many :reviews, dependent: :delete_all
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
       
   # validates :nickname, presence: true
-  validates :nickname, length: { maximum: 10 }
+  validates :nickname, presence: true, length: { maximum: 10 }
   validates :password, length: { maximum: 30 }
 end
