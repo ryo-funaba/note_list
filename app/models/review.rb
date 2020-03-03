3e7031e3b4e7dd0237dcec77d0036f564b13b621
ryo-funaba <ryo278.0417@gmail.com>
2020-03-03 13:47:10 +0000

    Reviewモデルの編集

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,7 +5,7 @@ class Review < ApplicationRecord
   attr_accessor :url
   
   validates :url, presence: true
-  validates :rate, presence: true
+  validates :rate, presence: true, numericality: { only_integer: true }
   validates :review, presence: true, length: { maximum: 400 }
   
   before_validation :exists_url
@@ -15,13 +15,13 @@ class Review < ApplicationRecord
   
   def exists_url
     # Noteレコードがあるかチェック
-    note = Note.find_by(url: "https://note.com/info/n/n5249d17f")
+    note = Note.find_by(url: url)
     return if note.present?
     # なければスクレイピングして必要な情報が取れるかチェック
-    product = Scraping.get_products("https://note.com/info/n1b2349d17f")
+    product = Scraping.get_products(url)
     # 情報が取れなければエラー
-    # return errors.add(:base, '入力されたURLは不正です') if product[:title].blank?
-    return "入力されたURLは不正です" if product[:title].blank?
+    return errors.add(:url, 'が不正です') if product[:title].blank?
+    # return "入力されたURLは不正です" if product[:title].blank?
   end
   
   def set_url
@@ -31,7 +31,7 @@ class Review < ApplicationRecord
       Scraping.get_products(url)
       # スクレイピング成功した場合
         # Noteレコードを作成
-        note = Note.create!() 
+      note = Note.create!() 
     end
     
     self.note = note
