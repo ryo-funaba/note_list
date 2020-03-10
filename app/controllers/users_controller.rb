b265929420a8e688dc5f3c30f23b17e58290b0c7
ryo-funaba <ryo278.0417@gmail.com>
2020-03-10 05:31:00 +0000

    ユーザー詳細画面 実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-    # @nickname = user.nickname
-    # notes = user.reviews.notes.page(params[:page]).per(8).order("created_at DESC")
+    @notes = Note.where(id: @user.reviews.select(:note_id)).page(params[:page]).per(7)
+    # binding.pry
   end
   
 end
