76915e870c33b79e3ca664c8d9d92c6b1e3764f7
ryo-funaba <ryo278.0417@gmail.com>
2020-03-20 07:17:23 +0000

    seeds編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+notes_csv = CSV.readlines("db/notes.csv")
+notes_csv.each do |row|
+  Note.create(url: row[1], title: row[2], title_image: row[3], creater: row[4], creater_image: row[5], like: row[6], price: row[7])
+end
