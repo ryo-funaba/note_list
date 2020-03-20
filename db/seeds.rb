caacf9fd7010c0e958aa9c31144e356259c725e1
ryo-funaba <ryo278.0417@gmail.com>
2020-03-20 07:55:11 +0000

    reviews.csv削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,3 @@ notes_csv = CSV.readlines("db/notes.csv")
 notes_csv.each do |row|
   Note.create(url: row[1], title: row[2], title_image: row[3], creater: row[4], creater_image: row[5], like: row[6], price: row[7])
 end
-
-reviews_csv = CSV.readlines("db/reviews.csv")
-reviews_csv.each do |row|
-  Review.create(user_id: row[1], note_id: row[2], rate: row[3], review: row[4])
-end
