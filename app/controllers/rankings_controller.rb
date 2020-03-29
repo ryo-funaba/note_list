0c43098b3abb82c775b1f24207debf16971ff50b
Matsushin <matsu1983@gmail.com>
2020-03-29 11:59:35 +0900

    N+1対応

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -4,7 +4,6 @@ class RankingsController < ApplicationController
   before_action :ranking
   
   def ranking
-    note_ids = Review.group(:note_id).order(count_note_id: :desc).limit(3).count(:note_id).keys
-    @ranking = note_ids.map{ |id| Note.find(id) }
+    @ranking = Review.includes(:note).select('note_id, count(*) as note_count').group(:note_id).order('note_count desc').limit(3)
   end
 end
