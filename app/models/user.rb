5f5f1d5eed8bc8e9d56c9ef8187a53e1f8792e6d
Matsushin <matsu1983@gmail.com>
2020-03-29 11:33:55 +0900

    モデルのリファクタ

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,18 +2,17 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   
-  has_one_attached :image 
+  has_one_attached :image
   has_many :reviews, dependent: :delete_all
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-      
-  # validates :nickname, presence: true
-  validates :nickname, presence: true, length: { minimum: 1,　maximum: 7 }
+
+  validates :nickname, presence: true, length: { minimum: 1 }
+  validates :nickname, length: { maximum: 7 }
   validates :image, presence: true
   
   def thumbnail
     return self.avatar.variant(resize: '50x50')
   end
-  
 end
