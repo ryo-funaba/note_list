e3be5f68c1effed93c677b4a9cf47e6f5fa07c1a
ryo-funaba <ryo278.0417@gmail.com>
2020-04-21 12:52:35 +0000

    features/signup_spec.rb修正

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -1,31 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe "ユーザー", type: :feature do
+RSpec.feature "ユーザー", type: :feature do
   
-  describe "新規登録" do
+  context "新規登録" do
 
-    it "ボタンの確認" do
+    scenario "ボタンの確認" do
       visit root_path
       expect(page).to have_content '新規登録'
     end
     
-    it "画面に遷移" do
+    scenario "画面に遷移" do
       visit root_path
       click_link "新規登録"
       expect(page).to have_content "メールアドレス"
     end
     
-    it "実行" do
+    scenario "実行" do
       visit new_user_registration_path
       fill_in "メールアドレス", with: "test@example.com"
       fill_in "パスワード", with: "password"
       fill_in "パスワード (確認)", with: "password"
       fill_in "ニックネーム", with: "test_user"
+      attach_file "プロフィール画像", "#{Rails.root}/spec/factories/rspec_profile.jpg"
       click_button '新規登録'
       expect(page).to have_content "サインアウト"
     end
     
-    it "画面からログイン画面に遷移" do
+    scenario "画面からログイン画面に遷移" do
       visit new_user_registration_path
       find("#login_link").click
       expect(page).to have_content "パスワードを忘れた方"
