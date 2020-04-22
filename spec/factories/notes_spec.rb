228be8cedcdaf7ab5cf9ea746bc4e64a8b2e58fd
ryo-funaba <ryo278.0417@gmail.com>
2020-04-22 02:08:43 +0000

    factories/notes/spec.rb 無駄な空白行の削除

--- spec/factories/notes_spec.rb
+++ spec/factories/notes_spec.rb
@@ -7,6 +7,5 @@ FactoryBot.define do
     creater_image { "https://assets.st-note.com/production/uploads/images/4411318/profile_53d5f3f9da8d9ec6a2a7b8712c59cdea.jpeg?fit=bounds&format=jpeg&quality=45&width=330" }
     like { 1675 }
     price { "¥1,980" }
-    
   end
 end
