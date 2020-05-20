3228f6fb02c9d48629a7db154d15b921d01c9ee5
ryo-funaba <ryo278.0417@gmail.com>
2020-05-20 17:05:19 +0900

    mysql ソケット設定

--- db/schema.rb
+++ db/schema.rb
@@ -46,15 +46,15 @@ ActiveRecord::Schema.define(version: 2020_04_28_071728) do
   end
 
   create_table "notes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "title"
-    t.string "title_image"
     t.string "creater"
+    t.string "url"
+    t.string "title_image"
     t.string "creater_image"
     t.integer "like"
     t.string "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
