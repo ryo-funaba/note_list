8c5a1cab242209d10c31275b7dc1b0790c916bb8
ryo-funaba <ryo278.0417@gmail.com>
2020-05-23 10:07:12 +0900

    toast通知 途中まで

---
+++ app/assets/javascripts/notification.js
@@ -0,0 +1,20 @@
+$(function(){
+
+  toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": true,
+    "progressBar": false,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": true,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "3000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+  }
+})
