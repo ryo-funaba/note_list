9e553e8478fdff50b84a6e219fff02a7eeceecc0
ryo-funaba <ryo278.0417@gmail.com>
2020-07-07 23:20:23 +0900

    add: Relationshipsのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-    
+
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  
+
   devise_for :users, controllers: { registrations: 'users/registrations' }
   root 'notes#index'
-  
+
   resources :users, only: :show
   resources :reviews, only: [:new, :create]
   resources :notes, only: [:index, :show] do
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
       get 'search'
     end
   end
+  resources :relationships, only: [:create, :destroy]
 end
