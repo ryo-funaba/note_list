4259ca0fcda0bfc457a86f4abbb573cfe5409105
ryo-funaba <ryo278.0417@gmail.com>
2020-07-08 23:23:40 +0900

    feat: Userモデルにdependent: :destroyを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
 
   has_one_attached :image
   has_many :reviews, dependent: :delete_all
-  has_many :relationships
+  has_many :relationships, dependent: :destroy
   has_many :followings, through: :relationships, source: :follow
-  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id', dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :user
 
   devise :database_authenticatable, :registerable,
