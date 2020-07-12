f2d8b6dd45bcfa7c8cdc0a1fadfdee56b443bb88
ryo-funaba <ryo278.0417@gmail.com>
2020-07-12 18:42:55 +0900

    feat: フォローボタンのhtmlファイル作成

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,14 +1,14 @@
 class NotesController < RankingsController
-  
+
   def index
     @notes = Note.order(created_at: :desc).page(params[:page]).per(7)
   end
-  
+
   def show
     @note = Note.find(params[:id])
     @reviews = @note.reviews.includes(:user)
   end
-  
+
   def search
    @notes = Note.where('title LIKE(?)', "%#{params[:keyword]}%").page(params[:page]).per(7)
   end
