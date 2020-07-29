2d0279451b728485c798ad7532e0e6dac3e460c1
ryo-funaba <ryo278.0417@gmail.com>
2020-07-29 23:41:04 +0900

    refactor: コメントアウト削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
   has_one_attached :image
   has_many :reviews, dependent: :delete_all
