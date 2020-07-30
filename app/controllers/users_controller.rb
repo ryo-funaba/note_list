fff9e3a177ff738348bda390d6941625e8378bd0
ryo-funaba <ryo278.0417@gmail.com>
2020-07-30 23:20:25 +0900

    refactor: コメントアウト削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,6 @@ class UsersController < RankingsController
   def show
     @user = User.find(params[:id])
     @notes = Note.where(id: @user.reviews.select(:note_id)).order("created_at DESC").page(params[:page]).per(7)
-    # binding.pry
   end
 
 end
