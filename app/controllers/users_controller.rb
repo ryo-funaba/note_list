cc8d4432c69de8901300b89c47da37f92813abf4
ryo-funaba <ryo278.0417@gmail.com>
2020-08-07 20:34:14 +0900

    refactor: before_action追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < RankingsController
 
+  before_action :set_user, only: [:show]
+
   def show
-    @user = User.find(params[:id])
     @notes = Note.where(id: @user.reviews.select(:note_id)).order("created_at DESC").page(params[:page]).per(7)
   end
 
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
